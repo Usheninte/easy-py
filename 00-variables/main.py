fd6e309af7bc62302bf33e7df262650a75075d2b
Ninte <ninte.dev@gmail.com>
2019-11-02 22:26:33 +0000

    added song details as variables to file

---
+++ 00-variables/main.py
@@ -0,0 +1,46 @@
+# Song title
+title = "HUMBLE."
+print(title)
+
+# Musician
+artiste = "Kendrick Lamar - as Artiste"
+print(artiste)
+
+"""
+'Humble' became Lamar's second number-one single on the US Billboard Hot 100 
+  after "Bad Blood" and his first as a lead artist. The song received four nominations 
+  at the 60th Annual Grammy Awards: Record of the Year, Best Rap Performance, 
+  Best Rap Song, and Best Music Video, winning the latter three.
+  Source: [https://en.wikipedia.org/wiki/Humble_(song)]
+"""
+
+# Songwriters
+writer1, writer2 = "Kendrick Lamar - as Writer", "Mike Will Made It - as Writer"
+print(writer1)
+print(writer2)
+
+# Mike Will Made It, co-wrote and produced the song!
+producedBy = "Mike Will Made It - as Producer"
+print(producedBy)
+
+# Music labels involved with song
+musicLabel1, musicLabel2, musicLabel3 = "Top Dawg", "Aftermath", "Interscope"
+print(musicLabel1)
+print(musicLabel2)
+print(musicLabel3)
+
+# Some certifications by country
+someCertifications = 0
+salesUSA = 8000000
+salesUK = 600000
+salesCanada = 560000
+someCertifications += salesUSA
+someCertifications += salesUK
+someCertifications += salesCanada
+print(someCertifications)
+
+# Release history
+firstReleaseWorldwide = "March 30, 2017"
+firstReleaseUSA = "April 4, 2017"
+print(firstReleaseWorldwide)
+print(firstReleaseUSA)
