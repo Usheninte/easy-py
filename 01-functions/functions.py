736d9e9e1246d95e01ce462b45f6b2b36293d2b9
Ninte <ninte.dev@gmail.com>
2019-11-09 11:41:14 +0000

    fulfilled assignment requirements

---
+++ 01-functions/functions.py
@@ -0,0 +1,25 @@
+def songWriters():
+    writer1, writer2 = "Kendrick Lamar - as Writer", "Mike Will Made It - as Writer"
+    print("Song Writers >>")
+    print(writer1)
+    print(writer2)
+# Function call
+songWriters()
+
+def musicLabels():
+    musicLabel1, musicLabel2, musicLabel3 = "Top Dawg", "Aftermath", "Interscope"
+    print("Music Labels >>")
+    print(musicLabel1)
+    print(musicLabel2)
+    print(musicLabel3)
+# Function call
+musicLabels()
+
+def releaseHistory():
+    firstReleaseWorldwide = "March 30, 2017"
+    firstReleaseUSA = "April 4, 2017"
+    print("Release History >>")
+    print(firstReleaseWorldwide)
+    print(firstReleaseUSA)
+# Function call
+releaseHistory()
